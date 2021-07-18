ba7e25892a10039000ee5ec76c31788bfea1f46d
Asko Soukka <asko.soukka@iki.fi>
2021-07-19 00:53:44 +0300

    Fix python CLI wrapper

--- carrot_rcc.py
+++ carrot_rcc.py
@@ -3,15 +3,14 @@ import subprocess
 import sys
 
 NODE_JS_EXECUTABLE = os.environ.get("NODE_JS_EXECUTABLE") or "node"
-CAMUNDA_ROBOT_BRIDGE_BUNDLE = os.environ.get(
-    "CAMUNDA_ROBOT_BRIDGE_BUNDLE"
-) or os.path.join(os.path.dirname(__file__), "index.js")
+CARROT_RCC_EXECUTABLE = os.environ.get("CARROT_RCC_EXECUTABLE") or os.path.join(
+    os.path.dirname(__file__), "carrot-rcc"
+)
 
 
 def main():
-    print("Hello World!")
-#    args = sys.argv[1:]
-#    subprocess.run([NODE_JS_EXECUTABLE] + list(args) + [CAMUNDA_ROBOT_BRIDGE_BUNDLE])
+    args = sys.argv[1:]
+    subprocess.run([NODE_JS_EXECUTABLE] + [CARROT_RCC_EXECUTABLE] + list(args))
 
 
 if __name__ == "__main__":
