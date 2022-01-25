478e5b886ab486fd1839242414b4679fbdca3906
Asko Soukka <asko.soukka@iki.fi>
2022-01-25 19:25:08 +0200

    vagrant: add to publish box with makefile

--- vagrant/default.nix
+++ vagrant/default.nix
@@ -94,7 +94,7 @@ in
         hyperv = {
            vmDerivationName = "nixos-vhdx-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}";
            vmFileName = "carrot-rcc-nixos-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}.vhdx";
-           baseImageSize = 8192;
+           baseImageSize = 9216;
         };
         boot.postBootCommands = ''
           # Provide a mount point for nixos-install.
